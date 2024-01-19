a8be1fbf490ff426cbc3fea15d96a9fbb5d9c49f
IVEN-CN <13377529851@stu.gdou.edu.cn>
2024-01-19 14:06:47 +0800

    封装两层cut

--- cutter/cut.py
+++ cutter/cut.py
@@ -3,11 +3,13 @@ import os
 import cv2
 import re
 
-def cut_img(path1200:str, path1000:str='./', path800:str='./'):
+def cut_img_(path1200:str, path1000:str='./', path800:str='./', elflag:bool=True):
     """将提供的800*1200尺寸的图片居中裁剪成800*800和750*1000
     path1200: 800*1200图片的路径
     path1000: 750*1000图片的路径
-    path800: 800*800图片的路径"""
+    path800: 800*800图片的路径
+    elflge: 是否对主图文件夹进行操作"""
+
     img = cv2.imread(path1200)
     if img.shape != (1200, 800, 3):
         raise ValueError('图片尺寸不正确')
@@ -25,6 +27,31 @@ def cut_img(path1200:str, path1000:str='./', path800:str='./'):
     cv2.imwrite(path1000, img2)
     cv2.imwrite(path800, img1)
 
+    if flag == 1 and elflag:
+        newname = newpath.replace('main', '主图')
+        os.rename(newpath, newname)
+
+def cut_img(path,filename, elflag=True):
+    global flag, newpath
+    flag = 0
+    if '主图' in path:
+        newpath = path.replace('主图', 'main')
+        os.rename(path, newpath)
+        flag = 1
+        _path = os.path.join(newpath, filename)
+        cut_img_(_path, newpath, newpath,elflag=elflag)
+    else:
+        _path = os.path.join(path, filename)
+        cut_img_(_path, path, path,elflag=elflag)
+
+def cut_cut(*filename, path):
+    for i in filename:
+        cut_img(path,i)
 
 if __name__ == '__main__':
-    cut_img('./1200(2).jpg')
+    # cut_img('D:\\41short\\KC-41-XOU132\\主图\\1200(5).jpg', 'D:\\41short\\KC-41-XOU132\\主图', 'D:\\41short\\KC-41-XOU132\\主图')
+    # cut_img('D:\\41short\\KC-41-XOU132\\主图\\1200(4).jpg', 'D:\\41short\\KC-41-XOU132\\主图', 'D:\\41short\\KC-41-XOU132\\主图')
+    # cut_img('./1200(7).jpg')
+    # cut_img('D:\\41short\\KC-41-XOU132\\主图','1200(5).jpg')
+    # cut_img('D:\\41short\\KC-41-XOU132\\主图','1200(4).jpg')
+    cut_cut('1200(5).jpg','1200(4).jpg',path=r'D:\41short\KC-41-XOU170\主图')
