a2b5348e724f4dd504320a9ec380a310ceb34796
IVEN-CN <13377529851@stu.gdou.edu.cn>
2024-01-26 11:33:15 +0800

    添加了贴图的功能，将套装png贴在jpg上

--- stack/stacker.py
+++ stack/stacker.py
@@ -3,7 +3,7 @@ import cv2
 import numpy as np
 import os
 
-def paste(a:np.ndarray, b:np.ndarray, position:tuple):
+def paste(a:np.ndarray, b:np.ndarray, position:tuple[int,int]):
     """将a数组贴在b数组上，position为贴图的左上角坐标"""
     shape = a.shape
 
@@ -12,27 +12,38 @@ def paste(a:np.ndarray, b:np.ndarray, position:tuple):
             b[position[0]+i][position[1]+k] = a[i][k]
     return b
 
-def paste_png_to_jpg(png_path, jpg_path, x, y):
+def paste_png_to_jpg(png_path, jpg_path, position:tuple[int,int]=(600, 750), test:bool=False):
 
     # 调整png图片的大小
     png = cv2.imread(png_path, cv2.IMREAD_UNCHANGED)
-    png = cv2.resize(png, (800, 800))
+    png = cv2.resize(png, (140, 76))
 
     # 读取jpg图片
     jpg = cv2.imread(jpg_path)
 
-    # 获取png图片的alpha通道
-    alpha = png[:, :, 3]
+    # 获取png图片的alpha通道，并转换为浮点数
+    alpha = png[:, :, 3].astype(float)/255
+
+    # 对alpha通道进行阈值处理，消除黑边
+    alpha = cv2.threshold(alpha, 0.5, 1, cv2.THRESH_BINARY)[1]
+
+    # 创建一个3通道的alpha图像
+    alpha = cv2.merge((alpha, alpha, alpha))
 
     # 获取png图片的rgb通道
-    rgb = png[:, :, :3]
+    rgb_png = png[:, :, :3]
 
-    # 分离RGB
-    B, G, R = cv2.split(rgb)
+    roi = jpg[position[1]:position[1]+png.shape[0], position[0]:position[0]+png.shape[1]]
 
-    cv2.imshow('jpg', jpg)
-    cv2.waitKey(0)
+    # 使用alpha通道作为掩码，将png图片贴在jpg图片上
+    jpg[position[1]:position[1]+png.shape[0], position[0]:position[0]+png.shape[1]] = (alpha * rgb_png + (1 - alpha) * roi).astype(np.uint8) # type: ignore
+
+    if test:
+        cv2.imshow('jpg', jpg)
+        cv2.waitKey(0)
+    else:
+        cv2.imwrite(jpg_path, jpg)
 
 
 if __name__ == '__main__':
-    paste_png_to_jpg(r'./blue.png', r'./1200(6).jpg', 0, 0)
+    paste_png_to_jpg(r'./blue.png', r'./1200(6).jpg', (600, 750),test=False)
